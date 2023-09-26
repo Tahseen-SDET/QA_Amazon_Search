0fd4fbccecaa6d09d96d0ae54f81ba9a0e2d294a
Tahseen yousuf <tehseenyousuf@gmail.com>
2023-09-26 15:34:44 +0530

    ME_XSEARCH1_MODULE_L1

--- src/main/java/apTests/TestCases.java
+++ src/main/java/apTests/TestCases.java
@@ -2,7 +2,7 @@
 package apTests;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
+//import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 //Selenium Imports
